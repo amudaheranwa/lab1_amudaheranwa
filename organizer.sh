73b8b806eac398ffca910838f19a82b915fd38d7
amudaheranwa <a.mudaheran@alustudent.com>
2026-07-24 20:26:19 +0000

    Finalised script

--- organizer.sh
+++ organizer.sh
@@ -5,3 +5,12 @@ if [ ! -d "archive" ]; then
 fi
 
 timestamp=$(date +"%Y%m%d-%H%M%S")
+new_filename="grades_${timestamp}.csv"
+ 
+mv grades.csv "archive/${new_filename}"
+ 
+touch grades.csv
+ 
+echo "${timestamp} | original: grades.csv | archived as: archive/${new_filename}" >> organizer.log
+ 
+echo "Archived grades.csv as archive/${new_filename}"
